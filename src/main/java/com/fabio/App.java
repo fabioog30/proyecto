93dd9d3d1bd0956ba34707a022ea5d8b4890fd92
fabioog30 <fortegag04@iesalbarregas.es>
2025-06-10 11:47:12 +0200

    Update App.java

--- src/main/java/com/fabio/App.java
+++ src/main/java/com/fabio/App.java
@@ -1,4 +1,3 @@
-// App.java corregido
 package com.fabio;
 
 import javafx.application.Application;
@@ -7,39 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class App extends Application {
-    private static Scene scene;
-    private static Stage primaryStage;
-
+    
     @Override
-    public void start(Stage stage) throws IOException {
-        primaryStage = stage;
-        try {
-            scene = new Scene(loadFXML("creacionPersonaje"), 640, 480);
-            stage.setScene(scene);
-            stage.setTitle("Creación de Personaje");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    public static Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/fabio/" + fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("creacionPersonaje.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
