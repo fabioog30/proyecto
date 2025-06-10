ca7108a4383a671d7ae5a0ea45ea9b8107ef9d19
fabioog30 <fortegag04@iesalbarregas.es>
2025-06-10 16:21:36 +0200

    Añadir variable maldición y las funciones Celda.java

--- src/main/java/com/fabio/modelo/Celda.java
+++ src/main/java/com/fabio/modelo/Celda.java
@@ -5,6 +5,7 @@ package com.fabio.modelo;
  */
 public class Celda {
     private boolean esPared;
+    private boolean esMaldicion;
     private Personaje personaje;
     
     /**
@@ -12,21 +13,36 @@ public class Celda {
      * 
      * @param esPared true si la celda es una pared, false si es suelo.
      */
-    public Celda(boolean esPared) {
+    public Celda(boolean esPared, boolean esMaldicion) {
         this.esPared = esPared;
         this.personaje = null;
+        this.esMaldicion = esMaldicion;
     }
     
     // Getters y setters
     public boolean esPared() { return esPared; }
+    public boolean esMaldicion() { return esMaldicion; }
     public boolean esTransitable() { return !esPared && personaje == null; }
     public Personaje getPersonaje() { return personaje; }
     
     public void setPersonaje(Personaje personaje) {
         this.personaje = personaje;
+
+        if(esMaldicion && personaje != null) {
+            aplicarMaldicion(personaje);
+        }
     }
     
     public void quitarPersonaje() {
         this.personaje = null;
+    }
+
+    private void aplicarMaldicion(Personaje personaje) {
+        // Reducir salud y salud máxima en un 25%
+        int nuevaSaludMaxima = (int)(personaje.getSaludMaxima() * 0.75);
+        int nuevaSalud = (int)(personaje.getSalud() * 0.75);
+        
+        personaje.recibirDanio(personaje.getSalud() - nuevaSalud);
+        personaje.setSaludMaxima(nuevaSaludMaxima);
     }
 }
